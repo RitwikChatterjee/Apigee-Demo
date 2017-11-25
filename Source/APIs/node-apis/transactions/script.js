661a924be4e9cc41eb9fae3c9f28aaf3683a795a
Ritwik Chatterjee <ritwik.t.chatterjee@gmail.com>
2017-11-25 13:13:39 -0700

    Modularize node-apis code. Changes to modularize node-apis code

--- Source/APIs/node-apis/transactions/script.js
+++ Source/APIs/node-apis/transactions/script.js
@@ -1,45 +1,25 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
 var port = process.env['PORT'] || 8080;
 
-// Gcloud example code START
-const config = {
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE
-};
-
-if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
-
-var connectionPool = mysql.createPool(config);
-// Gcloud example code END
-
+// Fetch data access handlers
+const handlers = require('./data-access/handlers');
 
 app.get('/transactions', function (req, res) {
+  // Fetching request data
   var acc_num = req.query.acc_num;
-  // var acc_num = req.params.acc_num;
-
   var start_dt = req.query.start_dt;
   var end_dt = req.query.end_dt;
-  connectionPool.getConnection(function(err, connection){
-    if (err) {
-      console.error(err);
+
+  // Send request to data access handlers
+  handlers.getTransactions(acc_num, start_dt, end_dt, function (error, rows) {
+    if (error) {
+      console.error("Error caught:", error);
+      res.send("Sorry, error received. Try back after sometime");
     } else {
-      console.log("Got connection");
-      connection.query("select * from Acc_Txn where acc_num ='" + acc_num + "'", function (error, rows, fields) {
-        connection.release();
-        if (error) {
-          console.error(error);
-        } else {
-          console.log("Success!!");
-          res.json(rows);
-        }
-      });
+      res.json(rows);
     }
-  });
-})
+  })
+});
 
 app.listen(port);
